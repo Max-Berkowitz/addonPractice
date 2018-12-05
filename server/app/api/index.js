68450584dfaa43c019dc9ee31f27be43efaea7bd
Max Berkowitz <maxl.berkowitz@gmail.com>
2018-12-04 18:28:38 -0800

    Update index.js

--- server/app/api/index.js
+++ server/app/api/index.js
@@ -6,13 +6,13 @@ const api = Router();
 api.get('/add', (req, res) => {
   const { num1, num2 } = req.query;
   const sum = add(+num1, +num2);
-  res.status(200).send({ sum: sum });
+  res.status(200).send({ sum });
 });
 
 api.get('/addPlusOne', (req, res) => {
   const { num1, num2 } = req.query;
   const sum = addPlusOne(+num1, +num2);
-  res.status(200).send({ sum: sum });
+  res.status(200).send({ sum });
 });
 
 module.exports = api;
